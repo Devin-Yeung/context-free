203cf7a0aac4534ba7df118481caf2588e2fabee
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-11-22 01:40:27 +0800

    feat: impl Display for SLRTable

--- src/slr/builder.rs
+++ src/slr/builder.rs
@@ -40,6 +40,12 @@ impl<'grammar> SLRTable<'grammar> {
     }
 }
 
+impl<'grammar> Display for SLRTable<'grammar> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("Table: \n {}", self.parsing_table()))
+    }
+}
+
 pub struct SLRTableBuilder<'grammar> {
     grammar: IndexedGrammar<'grammar>,
     follow: Follow<'grammar>,
