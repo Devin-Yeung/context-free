499d61472b35a9f7633e2b3f3b6b96336b209bdf
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-11-22 21:50:29 +0800

    feat: table view for first set

--- src/utils/first/mod.rs
+++ src/utils/first/mod.rs
@@ -1,6 +1,9 @@
 use crate::utils::first::builder::FirstBuilder;
 use bnf::{Grammar, Term};
+use itertools::Itertools;
 use std::collections::{HashMap, HashSet};
+use tabled::builder::Builder;
+use tabled::Table;
 
 mod builder;
 
@@ -11,5 +14,41 @@ pub struct First<'grammar> {
 impl<'grammar> First<'grammar> {
     pub fn new(grammar: &'grammar Grammar) -> First<'grammar> {
         FirstBuilder::new(grammar).build()
+    }
+
+    pub fn tabled(&self) -> Table {
+        let mut table = Builder::new();
+        table.set_header(["Term", "First(X)"]);
+        for (term, first) in self
+            .first
+            .iter()
+            .filter(|(term, _)| matches!(term, Term::Nonterminal(_)))
+            .sorted_by(|a, b| a.0.cmp(b.0))
+        {
+            table.push_record([term.to_string(), first.iter().sorted().join(", ")]);
+        }
+        table.build()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::utils::first::First;
+
+    #[test]
+    fn it_works() {
+        let grammar = r#"
+        <E> ::= <T> <E'>
+        <E'> ::= '+' <T> <E'> | 'ε'
+        <T> ::= <F> <T'>
+        <T'> ::= '*' <F> <T'> | 'ε'
+        <F> ::= '(' <E> ')' | 'id'
+        "#
+        .parse()
+        .unwrap();
+
+        let first = First::new(&grammar);
+
+        insta::assert_display_snapshot!(first.tabled());
     }
 }
