a49f8409f4ac127872ba46921d11b7af2fae3ee4
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-11-22 22:05:33 +0800

    feat: table view for follow set

--- src/utils/follow/mod.rs
+++ src/utils/follow/mod.rs
@@ -1,6 +1,9 @@
 use crate::utils::follow::builder::FollowBuilder;
 use bnf::{Grammar, Term};
+use itertools::Itertools;
 use std::collections::{HashMap, HashSet};
+use tabled::builder::Builder;
+use tabled::Table;
 
 mod builder;
 
@@ -16,5 +19,59 @@ impl<'grammar> Follow<'grammar> {
     pub fn follow_of(&self, x: &Term) -> impl Iterator<Item = &&'grammar Term> {
         // TODO: remove the unwrap?
         self.follow.get(x).unwrap().iter()
+    }
+
+    pub fn tabled(&self) -> Table {
+        let mut table = Builder::new();
+        table.set_header(["Term", "Follow(X)"]);
+        for (term, first) in self
+            .follow
+            .iter()
+            .filter(|(term, _)| matches!(term, Term::Nonterminal(_)))
+            .sorted_by(|a, b| a.0.cmp(b.0))
+        {
+            table.push_record([term.to_string(), first.iter().sorted().join(", ")]);
+        }
+        table.build()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::follow::Follow;
+    use bnf::Term;
+    use std::str::FromStr;
+
+    #[test]
+    fn it_works() {
+        let grammar = r#"
+        <E> ::= <T> <E'>
+        <E'> ::= '+' <T> <E'> | 'ε'
+        <T> ::= <F> <T'>
+        <T'> ::= '*' <F> <T'> | 'ε'
+        <F> ::= '(' <E> ')' | 'id'
+        "#
+        .parse()
+        .unwrap();
+        let start = Term::from_str("<E>").unwrap();
+        let follow = Follow::new(&grammar, &start);
+        insta::assert_display_snapshot!(follow.tabled());
+    }
+
+    #[test]
+    fn test_case_1() {
+        let grammar = r#"
+        <P> ::= <Q> 'id' <R>
+        <Q> ::= '∃' | '∀'
+        <R> ::= <E> '=' <E>
+        <E> ::= <T> <E'>
+        <E'> ::= '+' <T> <E'> | 'ε'
+        <T> ::= '(' <E> ')' | 'id'
+        "#
+        .parse()
+        .unwrap();
+        let start = Term::from_str("<P>").unwrap();
+        let follow = Follow::new(&grammar, &start);
+        insta::assert_display_snapshot!(follow.tabled());
     }
 }
