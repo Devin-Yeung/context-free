9a5e49ae1e15ab20a2af7ff988851513ee0dc362
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2023-11-23 15:07:36 +0800

    feat: add logger impl

--- examples/slr.rs
+++ examples/slr.rs
@@ -1,8 +1,13 @@
 use bnf::Production;
 use context_free::slr::builder::SLRTableBuilder;
+use log::LevelFilter;
 use std::str::FromStr;
 
 fn main() {
+    pretty_env_logger::formatted_builder()
+        .filter(None, LevelFilter::Debug)
+        .init();
+
     let grammar = r#"
         <S'> ::= <P>
         <P> ::= <Q> 'id' <R>
